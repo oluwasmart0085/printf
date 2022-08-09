a8cda10352e8bfc7e163faf1bce7408eebe8ff61
oluwasmart0085 <lebioluwasoji@gmail.com>
2022-08-09 15:38:20 -0700

    main h

--- main.h
+++ main.h
@@ -25,4 +25,24 @@ char *convert(unsigned long int num, int base, int lowercase);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size);
+int is_printable(char c);
+int append_hexa_code(char ascii_code, char buffer[], int i);
+int is_digit(char c);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_negative, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char buffer[],
+	int flags, int width, int prec,
+	int length, char padd, char extra_c);
+int write_unsgnd(int is_negative, int ind,
+	char buffer[],
+	int flags, int width, int precision, int size);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
 #endif
