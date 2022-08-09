0eb25b25169967370253cbc116fbb4deacea590f
oluwasmart0085 <lebioluwasoji@gmail.com>
2022-08-09 14:55:21 -0700

    main h

--- main.h
+++ main.h
@@ -21,5 +21,6 @@ int call_function_manager(char aux, va_list arg);
 int check_percent(int *flag, char aux);
 int get_flag(char s, flags_t *f);
 int (*get_print(char s))(va_list, flags_t *);
-char *convert(unsigned long int num, int base, int lowercase)
+char *convert(unsigned long int num, int base, int lowercase);
+int get_precision(const char *format, int *i, va_list list);
 #endif
